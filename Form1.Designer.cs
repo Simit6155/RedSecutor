b0c5e00091951c0fa750239cb20f310cc71cba64
semih <semihbiyikli769@gmail.com>
2025-11-26 09:44:00 +0100

    Enhance Form1 with new UI components
    Added additional UI elements including labels, buttons, and checkboxes to enhance functionality.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RedSecuterA
+namespace RedSecuterA
 {
     partial class Form1
     {
@@ -31,10 +31,21 @@
             button1 = new Button();
             button2 = new Button();
             richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            linkLabel1 = new LinkLabel();
+            checkBox1 = new CheckBox();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            label2 = new Label();
+            richTextBox2 = new RichTextBox();
+            button7 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
+            button1.Font = new Font("Segoe UI", 12F);
             button1.Location = new Point(12, 314);
             button1.Name = "button1";
             button1.Size = new Size(292, 31);
@@ -45,6 +56,8 @@
             // 
             // button2
             // 
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
             button2.Location = new Point(339, 314);
             button2.Name = "button2";
             button2.Size = new Size(292, 31);
@@ -57,18 +70,136 @@
             // 
             richTextBox1.BackColor = SystemColors.InactiveCaptionText;
             richTextBox1.ForeColor = SystemColors.MenuHighlight;
-            richTextBox1.Location = new Point(12, 12);
+            richTextBox1.Location = new Point(12, 30);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(619, 296);
+            richTextBox1.Size = new Size(619, 278);
             richTextBox1.TabIndex = 2;
             richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.ForeColor = SystemColors.Info;
+            label1.Location = new Point(12, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Made by - Simit6155 \r\n";
+            label1.Click += label1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = SystemColors.ActiveCaptionText;
+            linkLabel1.Font = new Font("Segoe UI", 10F);
+            linkLabel1.Location = new Point(379, 6);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(236, 19);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "https://www.instagram.com/redsimit/";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Checked = true;
+            checkBox1.CheckState = CheckState.Checked;
+            checkBox1.Cursor = Cursors.No;
+            checkBox1.Location = new Point(178, 8);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(649, 63);
+            button3.Name = "button3";
+            button3.Size = new Size(120, 28);
+            button3.TabIndex = 6;
+            button3.Text = "Infinite Yield";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(788, 63);
+            button4.Name = "button4";
+            button4.Size = new Size(120, 28);
+            button4.TabIndex = 7;
+            button4.Text = "Solara Hub V4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(649, 161);
+            button5.Name = "button5";
+            button5.Size = new Size(120, 28);
+            button5.TabIndex = 8;
+            button5.Text = "Arsenal";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(788, 161);
+            button6.Name = "button6";
+            button6.Size = new Size(120, 28);
+            button6.TabIndex = 9;
+            button6.Text = "Dex Explorer";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.ForeColor = SystemColors.Info;
+            label2.Location = new Point(679, 5);
+            label2.Name = "label2";
+            label2.Size = new Size(196, 28);
+            label2.TabIndex = 12;
+            label2.Text = "Popular Scripts/Hubs";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.BackColor = SystemColors.InactiveCaptionText;
+            richTextBox2.ForeColor = SystemColors.Highlight;
+            richTextBox2.Location = new Point(649, 218);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(259, 90);
+            richTextBox2.TabIndex = 13;
+            richTextBox2.Text = "";
+            // 
+            // button7
+            // 
+            button7.Location = new Point(743, 314);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 14;
+            button7.Text = "Redsecute";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaptionText;
-            ClientSize = new Size(643, 357);
+            ClientSize = new Size(920, 357);
+            Controls.Add(button7);
+            Controls.Add(richTextBox2);
+            Controls.Add(label2);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(checkBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(label1);
             Controls.Add(richTextBox1);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -76,6 +207,7 @@
             Text = "RedSecutor";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -83,5 +215,15 @@
         private Button button1;
         private Button button2;
         private RichTextBox richTextBox1;
+        private Label label1;
+        private LinkLabel linkLabel1;
+        private CheckBox checkBox1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Label label2;
+        private RichTextBox richTextBox2;
+        private Button button7;
     }
 }
